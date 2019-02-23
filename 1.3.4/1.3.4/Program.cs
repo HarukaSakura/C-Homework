076b4fc98b258f6bcaa4d381d1c4743ef7a88b5a
HarukaSakura <47911866+HarukaSakura@users.noreply.github.com>
2019-02-23 16:19:18 +0800

    Add files via upload

---
+++ 1.3.4/1.3.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            string s = "";
+            while (true)
+            {
+                Console.WriteLine("Please enter a number");
+                s = Console.ReadLine();
+                a = Double.Parse(s);
+                Console.WriteLine("Please enter another number");
+                s = Console.ReadLine();
+                b = Double.Parse(s);
+                c = a * b;
+                Console.WriteLine("The result is " + c +"\n");
+            }
+            
+        }
+    }
+}
