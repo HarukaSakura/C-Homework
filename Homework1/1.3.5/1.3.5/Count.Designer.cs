49277190626a7b205d42e0e7ab2adf775656476d
HarukaSakura <47911866+HarukaSakura@users.noreply.github.com>
2019-02-23 16:27:21 +0800

    Add files via upload

---
+++ Homework1/1.3.5/1.3.5/Count.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace _1._3._5
+{
+    partial class Count
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Count
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 473);
+            this.Name = "Count";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.Controls.Add(txt1);
+            this.Controls.Add(txt2);
+            this.Controls.Add(btn);
+            this.Controls.Add(lb1);
+            txt1.Dock = System.Windows.Forms.DockStyle.Left;
+            txt2.Dock = System.Windows.Forms.DockStyle.Right;
+            btn.Dock = System.Windows.Forms.DockStyle.Fill;
+            lb1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            btn.Text = "求积";
+            lb1.Text = "";
+            btn.Click += new System.EventHandler(this.button_Click);
+
+        }
+
+        #endregion
+        
+    }
+}
+
---
+++ Homework1/1.3.5/1.3.5/Count.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._3._5
+{
+    public partial class Count : Form
+    {
+
+        TextBox txt1 = new TextBox();
+        TextBox txt2 = new TextBox();
+        Button btn = new Button();
+        Label lb1 = new Label();
+        public Count()
+        {
+            InitializeComponent();
+        }
+        public void button_Click(object sender, EventArgs e)
+        {
+            string s = txt1.Text;
+            double n1 = double.Parse(s);
+            s = txt2.Text;
+            double n2 = double.Parse(s);
+            double result = n1 * n2;
+            lb1.Text = n1 + "和" + n2 + "的乘积是:" + result;
+        }
+    }
+}
