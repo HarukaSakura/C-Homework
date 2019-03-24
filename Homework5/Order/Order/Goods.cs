8861662b015e2e66fdd06ae9bc026d83db6caf95
HarukaSakura <47911866+HarukaSakura@users.noreply.github.com>
2019-03-24 17:25:19 +0800

    Add files via upload

---
+++ Homework5/Order/Order/Goods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order
+{
+    public class Goods
+    {
+        public string GoodsName { get; set; }
+        public int GoodsAmount { get; set; }
+        public float GoodsPrice { get; set; }
+        public Goods(string goodsName, int goodsAmount, float goodsprice)
+        {
+            this.GoodsName = goodsName;
+            this.GoodsAmount = goodsAmount;
+            this.GoodsPrice = goodsprice;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Goods)
+            {
+                Goods g = (Goods)obj;
+                if (g.GoodsName == GoodsName && g.GoodsAmount == GoodsAmount && g.GoodsPrice == GoodsPrice)
+                    return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Homework5/Order/Order/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService os = new OrderService();
+            while (true)
+            {
+                Console.WriteLine("选择服务：");
+                Console.WriteLine("1.添加订单");
+                Console.WriteLine("2.删除订单");
+                Console.WriteLine("3.查询订单");
+                Console.WriteLine("4.修改订单");
+                Console.WriteLine("5.所有订单概况（订单号排序）");
+                Console.WriteLine("6.所有订单概况（总金额排序）");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: os.CreateOrder(); break;
+                    case 2: os.CancelOrder(); break;
+                    case 3: os.QueryOrder(); break;
+                    case 4: os.ReviseOrder(); break;
+                    case 5: os.SortbyId(); break;
+                    case 6: os.SortbyTotalPrice(); break;
+                }
+                Console.WriteLine(" ");
+            }
+            
+        }
+    }
+}
